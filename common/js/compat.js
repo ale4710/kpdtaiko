c85a8f1f598ee20b5a84817e20ae8d03868500a6
ale4710 <alego4710@vivaldi.net>
2023-03-11 15:08:08 -0800

    various promise polyfills

--- common/js/compat.js
+++ common/js/compat.js
@@ -9,3 +9,20 @@ function getb2g() {
 if('b2g' in navigator) {
 	//stuff
 }
+
+//promise finally
+//	https://learnersbucket.com/examples/interview/promise-finally-polyfill/
+Promise.prototype.finally = function(callback) {
+	if (typeof callback !== 'function') {
+		return this.then(callback, callback);
+	}
+	// get the current promise or a new one
+	const P = this.constructor || Promise;
+
+	// return the promise and call the callback function
+	// as soon as the promise is rejected or resolved with its value
+	return this.then(
+		value => P.resolve(callback()).then(() => value),
+		err => P.resolve(callback()).then(() => { throw err; })
+	);
+};
