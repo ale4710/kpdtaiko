223b9d8886513c3098e768c897074b1e5e743879
ale4710 <alego4710@vivaldi.net>
2023-04-13 17:51:01 -0700

    small changes in waveform view

--- bottomstage/audioAnalyzer/drawWaveform.js
+++ bottomstage/audioAnalyzer/drawWaveform.js
@@ -1,14 +1,15 @@
 var _bottomStageAudioVisualizerDrawingFunction = (function(){
-	const waveformHeight = 0.75;
+	const waveformHeight = 0.8;
 	return function(ctx, color){
 		let by = ctx.canvas.height / 2;
 		let waveform = audioAnalyzer.getWaveform();
 		ctx.beginPath();
 		ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
 		ctx.strokeStyle = color;
+		//ctx.lineWidth = 2;
 		for(let i = 0; i < waveform.length; i++) {
 			let waveformPart = waveform[i];
-			let y = by + (((waveformPart / 128) - 1) * (waveformHeight * 0.5) * ctx.canvas.height);
+			let y = (by + (((waveformPart / 128) - 1) * (waveformHeight * 0.5) * ctx.canvas.height)) | 0;
 			if(i === 0) {
 				ctx.moveTo(0, y);
 			} else {
