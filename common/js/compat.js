104487b2804a2aea93204bf15e6493d2847d25c7
ale4710 <alego4710@vivaldi.net>
2023-04-14 18:24:01 -0700

    writing functions for device storage

--- common/js/compat.js
+++ common/js/compat.js
@@ -1,13 +1,13 @@
+function checkb2gns() {
+	return ('b2g' in navigator);
+}
+
 function getb2g() {
-	if('b2g' in navigator) {
+	if(checkb2gns()) {
 		return navigator.b2g;
 	} else {
 		return navigator;
 	}
-}
-
-if('b2g' in navigator) {
-	//stuff
 }
 
 //promise finally
--- common/js/deviceStorage.js
+++ common/js/deviceStorage.js
@@ -1,6 +1,7 @@
 var gameDirectory = '.domtaiko',
 gameSubDirectories = {
     songs: 'songs',
+	import: 'import',
     userMedia: 'custom'
 },
 deviceStorage = (getb2g().getDeviceStorage && getb2g().getDeviceStorage('sdcard')) || null,
@@ -38,6 +39,80 @@ function getFile(path) {
             reject(e.target);
         });
     }));
+};
+
+function addFile(path, blob) {
+	return new Promise((resolve, reject)=>{
+		console.log('writing',path);
+		let writeRequest = deviceStorage.addNamed(blob, path);
+		writeRequest.addEventListener('success', (ev)=>{
+			console.log('OK wrote:',path);
+			resolve();
+		}); 
+		writeRequest.addEventListener('error', (ev)=>{
+			reject(ev.target);
+		});
+	});
+};
+
+function deleteFile(path) {
+	return new Promise((resolve, reject)=>{
+		let delRequest = deviceStorage.delete(path);
+		delRequest.addEventListener('success', (ev)=>{
+			resolve();
+		});
+		delRequest.addEventListener('error', (ev)=>{
+			reject(ev.target);
+		});
+	});
+};
+
+function enumerateFiles(path, handlefn) {
+	return new Promise((resolve, reject)=>{
+		let dsEnum = deviceStorage.enumerate(path);
+		if(checkb2gns()) {
+			//3.0
+			let fileIterate = dsEnum.values();
+			function reciever(iterateRetv) {
+				if(iterateRetv.done) {
+					resolve();
+				} else {
+					let handlefnrv = handlefn(iterateRetv.value);
+					if(handlefnrv === 'stop') {
+						resolve();
+					} else {
+						next();
+					}
+				}
+			};
+			
+			function next() {
+				fileIterate.next()
+				.then(reciever)
+				.catch(reject);
+			};
+			
+			next();
+		} else {
+			//not 3.0
+			dsEnum.addEventListener('success', (ev)=>{
+				if(ev.target.done) {
+					resolve();
+				} else {
+					let handlefnrv = handlefn(ev.target.result);
+					if(handlefnrv === 'stop') {
+						resolve();
+					} else {
+						ev.target.continue();
+					}
+				}
+			});
+			
+			dsEnum.addEventListener('error', (ev)=>{
+				reject(ev);
+			});
+		}
+	});
 }
 
 function parseFilePath(filePath, relativeTo) {
