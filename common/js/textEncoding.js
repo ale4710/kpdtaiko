aadadb09994e64682e58b6a48a2ea61b1ca1d3e2
ale4710 <alego4710@vivaldi.net>
2023-04-17 18:21:52 -0700

    dont convert if already uint8array

--- common/js/textEncoding.js
+++ common/js/textEncoding.js
@@ -1,9 +1,11 @@
-function fixTextEncoding(arrayBuf) {
-    arrayBuf = new Int8Array(arrayBuf);
+function fixTextEncoding(buffer) {
+	if(!(buffer instanceof Uint8Array)) {
+		buffer = new Uint8Array(buffer);
+	}
     var utf8Decode = new TextDecoder('utf-8', {fatal: true}),
     parseAttempt;
     try {
-        parseAttempt = utf8Decode.decode(arrayBuf);
+        parseAttempt = utf8Decode.decode(buffer);
         return parseAttempt;
     } catch(e) {
 		console.error('failed to parse text (this is fine.)');
@@ -11,5 +13,5 @@ function fixTextEncoding(arrayBuf) {
     }
 
     var sjisDecode = new TextDecoder('shift-jis');
-    return sjisDecode.decode(arrayBuf);
+    return sjisDecode.decode(buffer);
 }
